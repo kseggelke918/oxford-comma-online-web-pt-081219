88c7f38623fd92be6d6f01b2606949f7613b7708
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-14 21:05:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 require "pry"
 
 def oxford_comma(array)
-  if array.length == 2 
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
     return array.join(" and ")
   elsif array.length >= 3 
     last_letter = array.pop
