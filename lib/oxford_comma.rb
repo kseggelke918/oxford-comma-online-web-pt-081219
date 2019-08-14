34594711986437b9b80ce4e2076c2e76a5dad533
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-14 20:59:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length >= 3 
     array.pop
-    new_array = "#{array}, and #{array.pop}"
+    new_array = ("#{array}, and #{array.pop}").join(",")
     
   end   
 end
